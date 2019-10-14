931a982dd35b134415fab960ad79aca1b865aa1f
ArvinthC3000 <54614142+ArvinthC3000@users.noreply.github.com>
2019-10-14 15:37:02 +0530

    .

--- main.c
+++ main.c
@@ -1,40 +1,65 @@
 #include <stdio.h>
 int main()
 {
-    int a[10][10], transpose[10][10], r, c, i, j;
-    printf("Enter rows and columns of matrix: ");
-    scanf("%d %d", &r, &c);
-    // Storing elements of the matrix
-    printf("\nEnter elements of matrix:\n");
+int a[10][10], first[10][10], second[10][10], third[10][10], r, c, i, j;
+int type;
+
+printf("Enter the number of rows and columns: ");
+scanf("%d %d", &r,&c);
+printf("\nEnter elements of matrix:\n");
+for(i=0; i<r; ++i)
+    for(j=0; j<c; ++j)
+    {
+        printf("Enter element a%d%d: ",i+1, j+1);
+        scanf("%d", &a[i][j]);
+    }
+printf("The entered matrix is\n \n");
+
     for(i=0; i<r; ++i)
-        for(j=0; j<c; ++j)
-        {
-            printf("Enter element a%d%d: ",i+1, j+1);
-            scanf("%d", &a[i][j]);
-        }
-    // Displaying the matrix a[][] */
-    printf("\nEntered Matrix: \n");
+    for(j=0; j<c; ++j)
+    {
+        printf("%d   ",a[i][j]);
+        if(j==c-1)
+            printf("\n");
+    }
     for(i=0; i<r; ++i)
-        for(j=0; j<c; ++j)
-        {
-            printf("%d  ", a[i][j]);
-            if (j == c-1)
-                printf("\n");
-        }
-    // Finding the transpose of matrix a
-    for(i=0; i<r; ++i)
-        for(j=0; j<c; ++j)
-        {
-            transpose[j][i] = a[i][j];
-        }
-    // Displaying the transpose of matrix a
-   printf("After single rotation \n");
-    for(i=0; i<c; ++i)
-        for(j=0; j<r; ++j)
-        {
-            printf("%d   ",transpose[i][(r-1)-j]);
-            if(j==r-1)
-                printf("\n");
-        }
-    return 0;
+    for(j=0; j<c; ++j)
+    {
+        first[j][i] = a[i][j];
+    }
+printf("After single rotation \n");
+for(i=0; i<c; ++i)
+    for(j=0; j<r; ++j)
+    {
+        printf("%d   ",first[i][(r-1)-j]);
+        if(j==r-1)
+            printf("\n");
+    }
+  for(i=0; i<c; ++i)
+    for(j=0; j<r; ++j)
+    {
+        second[j][i] = first[i][(r-1)-j];
+    }
+printf("After second rotation \n");
+for(i=0; i<r; ++i)
+    for(j=0; j<c; ++j)
+    {
+        printf("%d   ",second[i][(c-1)-j]);
+        if(j==c-1)
+            printf("\n");
+    }
+  for(i=0; i<r; ++i)
+    for(j=0; j<c; ++j)
+    {
+        third[j][i] = second[i][(c-1)-j];
+    }
+printf("After third rotation \n");
+for(i=0; i<c; ++i)
+    for(j=0; j<r; ++j)
+    {
+        printf("%d   ",third[i][(r-1)-j]);
+        if(j==r-1)
+            printf("\n");
+    }
+return 0;
 }
